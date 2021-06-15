943780fe5c9cabcc4bc3b3b96f17d080450bd7ad
rjmtl <59107379+rjmtl@users.noreply.github.com>
2021-06-15 18:17:40 +0530

    Admin Functionalities

--- src/Admin/admin.js
+++ src/Admin/admin.js
@@ -1,31 +1,31 @@
 import { useState, useEffect } from "react";
+import "./admin.css";
 
 function Admin(props) {
-var temp_arr=[];
-    if(localStorage["Trial"]){
-  temp_arr = JSON.parse(localStorage["Trial"]);
-    }
+  var temp_arr = [];
+  if (localStorage["Trial"]) {
+    temp_arr = JSON.parse(localStorage["Trial"]);
+  }
 
-   const[currentItem,setCurrentItem]=useState();
+  const [currentItem, setCurrentItem] = useState();
   const [item, setItem] = useState(temp_arr);
   const [name, setName] = useState("");
   const [price, setPrice] = useState("");
   const [image, setImage] = useState("");
   const [desc, setDesc] = useState("");
 
-
-
-  const submithandle=(e)=> {
+  const submithandle = (e) => {
     e.preventDefault();
     var data = {
-      title:name,
+      title: name,
       price,
       image,
-      description:desc,
+      description: desc,
     };
 
-    setItem([...item,data]);
-
+    setItem([...item, data]);
+    alert("Item Added Successfully");
+    document.querySelector(".main_product").classList.remove('visible');
   };
 
   useEffect(() => {
@@ -45,35 +45,67 @@ var temp_arr=[];
   };
   return (
     <>
-      <h1>Add products</h1>
-      <form onSubmit={(e)=>{submithandle(e)}}>
+      <div className="button_main">
+        <div className="buttons">
+          <button className="main_buttons" onClick={()=>document.querySelector(".main_product").classList.add('visible')}>Add Products</button>
+          <button className="main_buttons" onClick={() => props.history.push("/admin_product_listing")}>
+          View Products
+        </button>
+
+
+        </div>
+      </div>
+<div className="main_product">
+  <br/>
+  <span className="close" onClick={()=>{
+document.querySelector(".main_product").classList.remove('visible');
+  }}>x</span>
+      <form
+        className="add_product"
+        onSubmit={(e) => {
+          submithandle(e);
+        }}
+      >
+        <label className="label">Product Title</label>
+        <br/>
         <input
-          placeholder="Name of Product"
+          className="product_prop"
+          // placeholder="Name of Product"
           onChange={(e) => setName(e.target.value)}
         />
         <br />
+        <label className="label">Price</label>
+        <br/>
         <input
-          placeholder="Price in INR"
+          className="product_prop"
+          // placeholder="Price in INR"
           type="number"
           onChange={(e) => setPrice(e.target.value)}
         />
         <br />
+        <label className="label">Description</label>
+        <br/>
         <input
-          placeholder="Description"
+          className="product_prop"
+          // placeholder="Description"
           onChange={(e) => setDesc(e.target.value)}
         />
         <br />
+        <label className="label">Product Image</label>
+        <br/>
         <input
           type="file"
+          className="product_prop"
           accept="image/gif, image/jpeg, image/png"
           name="image"
           id="file"
           onChange={(e) => image_handle(e)}
         />
         <br />
-        <button type="submit">Add Product</button>
-        <button onClick={()=>props.history.push("/admin_product_listing")}>View Products</button>
+        <button className="button" type="submit">Add Product</button>
+
       </form>
+      </div>
     </>
   );
 }
--- src/Admin/admin_display.js
+++ src/Admin/admin_display.js
@@ -1,25 +1,140 @@
-import "./admin.css"
+import { useEffect,useState } from "react";
+import "./admin.css";
 
-function AdminDisplay(){
-let arr=JSON.parse(localStorage["Trial"]);
+function AdminDisplay() {
+  var arr=JSON.parse(localStorage["Trial"]);
+  const [item,setItem]=useState(arr);
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState("");
+  const [image, setImage] = useState("");
+  const [desc, setDesc] = useState("");
+  const [buttonVal,setter]=useState("");
 
-return(
-<>
-<h1>Product Listing</h1>
-{
-    arr.map((value,index)=>
-        <div className="Admin_Display">
-            <img src={value.image} width="100%" height="100%"/>
+
+  const [products,setProducts]=useState([]);
+
+  const image_handle = (e) => {
+    var image = e.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(image);
+    reader.onload = () => {
+      image = reader.result;
+      setImage(image);
+    };
+  };
+
+  const submithandle=(e)=>{
+    e.preventDefault();
+    var data = {
+      title: name,
+      price,
+      image,
+      description: desc,
+    };
+
+    document.querySelector(".main_product").classList.remove('visible');
+    arr[buttonVal]=data;
+    console.log(arr);
+    localStorage.setItem("Trial",JSON.stringify(arr));
+    setProducts(arr);
+  }
+
+const edithandle=(value,index)=>{
+
+console.log(value);
+setName(value.title);
+setPrice(value.price);
+setDesc(value.description);
+setter(index);
+document.querySelector(".main_product").classList.add('visible');
+  }
+
+  const deletehandle=(index)=>{
+    arr.splice(index,1);
+    localStorage.setItem("Trial",JSON.stringify(arr));
+    setProducts(arr);
+  }
+
+  return (
+    <>
+      {/* <h1>Product Listing</h1> */}
+      <div className="main_product">
+  <br/>
+  <span className="close" onClick={()=>{
+document.querySelector(".main_product").classList.remove('visible');
+  }}>x</span>
+      <form
+        className="add_product"
+        onSubmit={(e) => {
+          submithandle(e);
+        }}
+      >
+        <label className="label">Product Title</label>
+        <br/>
+        <input
+          className="product_prop"
+          value={name}
+          // placeholder="Name of Product"
+          onChange={(e) => setName(e.target.value)}
+        />
+        <br />
+        <label className="label">Price</label>
+        <br/>
+        <input
+          className="product_prop"
+          value={price}
+          // placeholder="Price in INR"
+          type="number"
+          onChange={(e) => setPrice(e.target.value)}
+        />
+        <br />
+        <label className="label">Description</label>
+        <br/>
+        <input
+          className="product_prop"
+          value={desc}
+          // placeholder="Description"
+          onChange={(e) => setDesc(e.target.value)}
+        />
+        <br />
+        <label className="label">Product Image</label>
+        <br/>
+        <input
+          type="file"
+          className="product_prop"
+          accept="image/gif, image/jpeg, image/png"
+          name="image"
+          id="file"
+          // value={image}
+          onChange={(e) => image_handle(e)}
+        />
+        <br />
+        <button className="button" type="submit">Save</button>
+
+      </form>
+      </div>
+
+      <div className="main_admin">
+        {arr.map((value, index) => (
+
+          <div className="Admin_Display">
+            <img src={value.image} className="image" />
             <p>{value.title}</p>
-            <p>{value.description}</p>
             <p>{value.price}</p>
-        </div>
-    )
-
-
+            {
+            <button onClick={()=>{deletehandle(index)}}>Delete</button>
 }
-</>
-)
+            <br/>
+            <br/>
+            <button onClick={()=>{edithandle(value,index)}}>Edit </button>
+            <br/>
+          </div>
+
+
+        ))}
+      </div>
+    </>
+  );
 }
 
 export default AdminDisplay;
